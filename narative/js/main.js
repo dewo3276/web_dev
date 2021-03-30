9d334b8ef1a03b54f5e5617ccaa3b21b42cecbc5
dewo3276 <44533218+dewo3276@users.noreply.github.com>
2021-03-30 16:01:46 -0600

    Add files via upload

---
+++ narative/js/main.js
@@ -0,0 +1,10 @@
+/*var current = $(window).scrollTop();
+var total = $(window).height() - current;
+var ele = $(".parallax__layer--spaceShip");
+var currPosition = ele.position().left + 500;
+var trackLength = 300;
+$(window).scroll(function (event) {
+    current = $(window).scrollTop();
+    var newPosition = trackLength * (current/total)
+    ele.css({left:currPosition+newPosition+'px'});
+});*/
