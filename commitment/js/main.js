957b3aa0dd1df49fb79e4b07ae9f3a1ff1ad9586
Destin Woods <dewo3276@colorado.edu>
2021-04-28 10:18:17 -0600

    wrong file path

--- commitment/js/main.js
+++ commitment/js/main.js
@@ -7,19 +7,19 @@ let research = document.getElementById('messages');
 function changeImageGif(value) {
   switch (value) {
     case 0:
-    changer.src = "../img/potato_upRight.gif"
+    changer.src = "img/potato_upRight.gif"
     document.getElementById("researchpara").style.display="block"
       break;
     case 1:
-    changer.src = "../img/potato_upLeft.gif"
+    changer.src = "img/potato_upLeft.gif"
     document.getElementById("drinkingpara").style.display="block"
       break;
     case 2:
-    changer.src = "../img/potato_downRight.gif"
+    changer.src = "img/potato_downRight.gif"
     document.getElementById("textilespara").style.display="block"
       break;
     case 3:
-    changer.src = "../img/potato_downLeft.gif"
+    changer.src = "img/potato_downLeft.gif"
     document.getElementById("messagespara").style.display="block"
       break;
     default:
@@ -36,7 +36,7 @@ function holyAnimation()
 
 function showfatman()
 {
-  document.getElementById('godPotato').style.backgroundImage="url('../img/fire.gif')"
+  document.getElementById('godPotato').style.backgroundImage="url('img/fire.gif')"
   document.getElementById('runawayIMG').style.display="block"
   document.getElementById('demonPotato').style.display="block"
 }
